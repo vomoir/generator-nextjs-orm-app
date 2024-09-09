937eec06245b26a4557eac5e0130b1e254e6eb1b
Tony Edwards <anthonybedwards@optusnet.com.au>
2024-09-09 18:21:17 +1000

    added sqlite

--- generators/app/index.js
+++ generators/app/index.js
@@ -23,43 +23,138 @@ export default class extends Generator {
                 type: 'input',
                 name: 'componentName',
                 message: 'Your Component name',
-                default: this.appname // Default to current folder name
+                default: this.appname, // Default to current folder name
+                store: true
+            },
+            {
+                type: "confirm",
+                name: "srcDir",
+                message: "Use a 'src' folder?",
+                default: false,
+                sore: true
+
             },
             {
                 type: 'input',
                 name: 'assetsFolder',
                 message: 'Folder name for application assets (e.g., images)',
-                default: 'public' // Default folder name for assets
+                default: 'public', // Default folder name for assets
+                store: true
             },
             {
-                type: 'input',
-                name: 'githubUsername',
-                message: 'GitHub username:',
-                default: githubUsername,
+                type: "confirm",
+                name: "includeInstall",
+                message: "Install dependencies?",
+                default: true,
             },
+
+            // {
+            //     type: 'input',
+            //     name: 'githubUsername',
+            //     message: 'GitHub username:',
+            //     default: githubUsername,
+            //     store: true
+            // },
         ]);
     }
 
     writing() {
         const { componentName, assetsFolder } = this.answers;
-        this.log(assetsFolder);
+        this.log(`Assets folder: ${assetsFolder}`);
         this.destinationRoot(this.destinationPath(componentName));
-        this.fs.write(this.destinationPath('README.md'), `# ${componentName}`);
+
+        this.fs.copy(
+            this.templatePath("env/gitignore"),
+            this.destinationPath(".gitignore")
+        );
+
+        this.fs.copy(
+            this.templatePath("env/editorconfig"),
+            this.destinationPath(".editorconfig")
+        );
+
+        this.fs.copy(
+            this.templatePath("env/eslintignore"),
+            this.destinationPath(".eslintignore")
+        );
+
+        this.fs.copy(
+            this.templatePath("env/tsconfig.json"),
+            this.destinationPath("tsconfig.json")
+        );
+
+        this.fs.copy(
+            this.templatePath("env/tailwind.config.ts"),
+            this.destinationPath("tailwind.config.ts")
+        );
+
+        this.fs.copy(
+            this.templatePath("env/prettier.config.js"),
+            this.destinationPath("prettier.config.js")
+        );
+
+        // Creates a file from scratch: 
+        this.fs.write(this.destinationPath('FRUNOBULAX.txt'), `# ${componentName}`);
+
         this.fs.copyTpl(
             this.templatePath('Component.js'),
             this.destinationPath(`src/components/${componentName}.js`),
             { componentName: componentName }
         );
 
-        // Create the assets folder
+        this.fs.copyTpl(
+            this.templatePath('package.json'),
+            this.destinationPath('package.json')
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('postcss.config.js'),
+            this.destinationPath('postcss.config.js')
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('README.md'),
+            this.destinationPath('README.md'),
+            { componentName: componentName }
+        );
+
+        // this.fs.copyTpl(
+        //     this.templatePath('hero-desktop.png'),
+        //     this.destinationPath('hero-desktop.png')
+        // );
+
+        // Create the assets/public folder
         this.fs.copy(
-            this.templatePath('assets'),
+            this.templatePath(`${assetsFolder}`),
             this.destinationPath(`${assetsFolder}`)
         );
 
         this.fs.copy(
-            this.templatePath('app/dashboard'),
-            this.destinationPath('src/app/dashboard')
+            this.templatePath("core/**/*"),
+            this.answers.srcDir ? this.destinationPath(`src/app`) : this.destinationPath(`app`)
         );
+
+        // Replace name in package.json with selected project name
+        const packageJson = this.fs.readJSON(
+            this.destinationPath("package.json")
+        );
+        packageJson.name = "@vomsoft/" + componentName;
+        this.fs.writeJSON(
+            this.destinationPath("package.json"),
+            packageJson
+        );
+    }
+    //  Initialise the node dependencies
+    install() {
+        if (this.answers.includeInstall) {
+            this.spawnSync("npm", ["install"]);
+        }
+    }
+
+    end() {
+        this.log('******************************************')
+        this.log(`Run the project: cd ${this.answers.componentName} then 'npm run dev'`)
+        this.log(`Seed the SQLite db: cd ${this.answers.componentName}/app then 'node connect.js'`)
+        this.log('******************************************')
     }
 }
